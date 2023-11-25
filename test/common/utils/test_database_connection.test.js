413288925b1341c4449473d121e4d2c61d328d41
quangpao <ddquangbao@gmail.com>
2023-11-25 20:48:33 +0700

    Fix database connection test and handle disconnect failure

--- test/common/utils/test_database_connection.test.js
+++ test/common/utils/test_database_connection.test.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const Database = require('../../../src/common/utils/Database')
 const { expect } = require('chai')
 const Sinon = require('sinon')
-const Logger = require('../../../src/common/classes/Logger')
 
 // Test suite
 describe('Database', function () {
@@ -12,14 +11,14 @@ describe('Database', function () {
 
     let sandbox
 
-    // before(() => {
-    //   sandbox = Sinon.stub(mongoose, 'connect').resolves().callsFake(() => {
-    //     mongoose.connection.readyState = 1
-    //   })
-    // })
-    // after(() => {
-    //   sandbox.restore()
-    // })
+    before(() => {
+      sandbox = Sinon.stub(mongoose, 'connect').resolves().callsFake(() => {
+        mongoose.connection.readyState = 1
+      })
+    })
+    after(() => {
+      sandbox.restore()
+    })
 
     it('should connect to the database', async () => {
 
@@ -72,6 +71,29 @@ describe('Database', function () {
       await Database.disconnect()
       // Check if the disconnection is successful
       expect(mongoose.connection.readyState).to.equal(0)
+    })
+  })
+
+  describe('Database.disconnect() function failure', () => {
+
+    let sandbox, exitSandbox
+
+    before(() => {
+      sandbox = Sinon.stub(mongoose, 'disconnect').rejects()
+      exitSandbox = require('sinon').stub(process, 'exit').callsFake(() => {
+        process.exitCode = 1
+      })
+    })
+    after(() => {
+      sandbox.restore()
+      exitSandbox.restore()
+    })
+
+    it('should not disconnect from the database', async () => {
+      await Database.disconnect()
+
+      expect(exitSandbox.calledOnce).to.be.true
+      expect(process.exitCode).to.equal(1)
     })
   })
 
