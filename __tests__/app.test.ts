e2236e617e200e8ec547686b5de47f8587b9ebbe
quangpao <ddquangbao@gmail.com>
2024-01-29 17:23:26 +0700

    Delete unused files and add app.ts and app.test.ts

---
+++ __tests__/app.test.ts
@@ -0,0 +1,8 @@
+
+describe('getRandomNumber', () => {
+    it('should return a random number between 1 and 10', () => {
+        const randomNumber = Math.floor(Math.random() * 10) + 1;;
+        expect(randomNumber).toBeGreaterThanOrEqual(1);
+        expect(randomNumber).toBeLessThanOrEqual(10);
+    });
+});
--- src/common/utils/Config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({ path: '/app/.env' })
-
-module.exports = {
-  TOKENID: process.env.TOKENID || false,
-  CLIENTID: process.env.CLIENTID || false,
-  MONGO_URI: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}/?retryWrites=true&w=majority`,
-}
