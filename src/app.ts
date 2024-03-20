10d65a08e6d15921d4bc540f133b8ad803685cfc
quangpao <ddquangbao@gmail.com>
2024-03-20 11:51:42 +0700

    [GENEX-29] Add `bun-build` workflow

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
 
-const client = new Client({intents: [GatewayIntentBits.Guilds]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, (c) => {
-    console.log(`Logged in as ${c.user?.tag}`);
-})
-client.login(process.env.TOKENID)
+  console.log(`Logged in as ${c.user?.tag}`);
+});
+client.login(process.env.TOKENID);
